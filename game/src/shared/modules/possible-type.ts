1db9241eee8b0ce2f26bbe0383184845b5208e8c
AshwinHub <phantomfills@outlook.com>
2023-09-01 16:29:12 +0100

    🔨created optionalToPossible utility function
    - wraps a value in a possible type

--- game/src/shared/modules/possible-type.ts
+++ game/src/shared/modules/possible-type.ts
@@ -6,3 +6,14 @@ export type Possible<T> =
 	| {
 			exists: false;
 	  };
+
+export const optionalToPossible = <T>(value: T): Possible<T> => {
+	if (value === undefined)
+		return {
+			exists: false,
+		};
+	return {
+		exists: true,
+		value: value,
+	};
+};
