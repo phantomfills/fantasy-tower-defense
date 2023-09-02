b3b7456c0e1743d6d60df8efcb2006a580019d1b
AshwinHub <phantomfills@outlook.com>
2023-09-02 12:26:57 +0100

    🔨bug fix - archer not parented to workspace

--- game/src/server/modules/archer.ts
+++ game/src/server/modules/archer.ts
@@ -1,11 +1,12 @@
 import { GenericTowerStats, Tower } from "./tower";
-import { ReplicatedStorage } from "@rbxts/services";
+import { ReplicatedStorage, Workspace } from "@rbxts/services";
 
 const archerModel = ReplicatedStorage.assets.towers.archer.models.archer;
 
 export class Archer extends Tower<GenericTowerStats> {
 	constructor(cframe: CFrame) {
 		const archer = archerModel.Clone();
+		archer.Parent = Workspace;
 
 		super(archer, {
 			damage: 10,
