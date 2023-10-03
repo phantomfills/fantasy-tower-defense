952b504a1cc0d3fa92338c587785aa393b628658
AshwinHub <phantomfills@outlook.com>
2023-10-03 19:28:27 +0100

    🔨fix zindex issue with cash label

--- game/src/client/ui/cash-label.tsx
+++ game/src/client/ui/cash-label.tsx
@@ -8,11 +8,16 @@ export interface CashLabelProps {
 }
 
 export const CashLabel = (props: CashLabelProps) => {
-	const { value, size, position } = props;
+	const { value, size, position, zIndex } = props;
 
 	return (
-		<frame Size={size} Position={position} BackgroundTransparency={1} ZIndex={props.zIndex}>
-			<imagelabel Image="rbxassetid://1022613008" Size={new UDim2(0.35, 0, 1, 0)} BackgroundTransparency={1} />
+		<frame Size={size} Position={position} BackgroundTransparency={1} ZIndex={zIndex}>
+			<imagelabel
+				Image="rbxassetid://1022613008"
+				Size={new UDim2(0.35, 0, 1, 0)}
+				BackgroundTransparency={1}
+				ZIndex={zIndex}
+			/>
 			<textlabel
 				Size={new UDim2(0.65, 0, 1, 0)}
 				Position={new UDim2(0.35, 0, 0, 0)}
@@ -22,6 +27,7 @@ export const CashLabel = (props: CashLabelProps) => {
 				TextXAlignment={Enum.TextXAlignment.Center}
 				Font={Enum.Font.GothamBold}
 				Text={tostring(value)}
+				ZIndex={zIndex}
 			>
 				<uistroke Color={Color3.fromRGB(0, 0, 0)} Thickness={2} />
 			</textlabel>
