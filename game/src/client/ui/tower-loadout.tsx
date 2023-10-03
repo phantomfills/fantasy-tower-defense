e2f5f67f5e79983df9ef226fcb76935d4f8737c9
AshwinHub <phantomfills@outlook.com>
2023-10-03 19:28:53 +0100

    🔨decrease size and add padding to tower loadout

--- game/src/client/ui/tower-loadout.tsx
+++ game/src/client/ui/tower-loadout.tsx
@@ -7,9 +7,9 @@ interface TowerLoadoutProps {
 
 export const TowerLoadout = (props: TowerLoadoutProps) => {
 	return (
-		<frame Position={new UDim2(0, 0, 0.8, 0)} BackgroundTransparency={1} Size={new UDim2(1, 0, 0.2, 0)}>
+		<frame Position={new UDim2(0, 0, 0.8, -5)} BackgroundTransparency={1} Size={new UDim2(1, 0, 0.2, 0)}>
 			<uigridlayout
-				CellSize={new UDim2(0.1, 0, 1, 0)}
+				CellSize={new UDim2(0.075, 0, 1, 0)}
 				CellPadding={new UDim2(0.025, 0, 0, 0)}
 				HorizontalAlignment={Enum.HorizontalAlignment.Center}
 			/>
