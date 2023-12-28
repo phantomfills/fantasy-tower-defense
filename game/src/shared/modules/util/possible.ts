2720531f5fdb23680295c68ef03090e905c5bb5d
AshwinHub <phantomfills@outlook.com>
2023-12-28 16:47:21 +0000

    ♻reverse ternary operator in possible function and change generic type to be more specific

--- game/src/shared/modules/util/possible.ts
+++ game/src/shared/modules/util/possible.ts
@@ -1,12 +1,12 @@
-export type Possible<T> =
+export type Possible<ValueType> =
 	| {
 			exists: true;
-			value: T;
+			value: ValueType;
 	  }
 	| {
 			exists: false;
 	  };
 
-export function possible<T>(value: T | undefined): Possible<T> {
-	return value !== undefined ? { exists: true, value } : { exists: false };
+export function possible<ValueType>(value: ValueType | undefined): Possible<ValueType> {
+	return value === undefined ? { exists: false } : { exists: true, value };
 }
