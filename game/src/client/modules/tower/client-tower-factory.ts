f067672b8927d608e849ecab92053942b6b97968
AshwinHub <phantomfills@outlook.com>
2024-03-16 21:13:54 +0000

    🔨change client archer factory to use new client archer class

--- game/src/client/modules/tower/client-tower-factory.ts
+++ game/src/client/modules/tower/client-tower-factory.ts
@@ -1,5 +1,5 @@
 import { TowerType } from "shared/modules/tower/tower-type";
-import { ClientArcher } from "./client-archer";
+import { ClientArcher0 } from "./client-archer-0";
 import { GenericClientTower } from "./client-tower";
 import { ClientArcher1 } from "./client-archer-1";
 
@@ -7,7 +7,7 @@ export function createClientTower(towerType: TowerType, level: number, id: strin
 	switch (towerType) {
 		case "ARCHER": {
 			if (level === 0) {
-				return new ClientArcher(id, cframe);
+				return new ClientArcher0(id, cframe);
 			} else {
 				return new ClientArcher1(id, cframe);
 			}
