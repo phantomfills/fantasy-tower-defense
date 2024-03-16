f0d4b1418410a9ab8a90c1b84b731d55d477efda
AshwinHub <phantomfills@outlook.com>
2024-03-16 21:11:48 +0000

    🔨introduce levels to tower model map

--- game/src/shared/modules/tower/tower-type-to-model-map.ts
+++ game/src/shared/modules/tower/tower-type-to-model-map.ts
@@ -2,10 +2,17 @@ import { TowerModel } from "./tower-model";
 import { TowerType } from "./tower-type";
 import { ReplicatedStorage } from "@rbxts/services";
 
-const towerTypeToModelMap: Record<TowerType, TowerModel> = {
-	ARCHER: ReplicatedStorage.assets.towers.archer.models.archer,
-};
+const assets = ReplicatedStorage.assets;
+const archerModels = assets.towers.archer.models;
 
-export function getTowerModelFromType(_type: TowerType) {
-	return towerTypeToModelMap[_type];
+const towerTypeToModelsMap = {
+	ARCHER: [archerModels.level_0, archerModels.level_1],
+} satisfies Record<TowerType, TowerModel[]>;
+
+export function getTowerPlacementModelFromType(_type: TowerType) {
+	return towerTypeToModelsMap[_type][0].Clone();
+}
+
+export function getTowerModelFromTypeAndLevel(_type: TowerType, level: number) {
+	return towerTypeToModelsMap[_type][level].Clone();
 }
