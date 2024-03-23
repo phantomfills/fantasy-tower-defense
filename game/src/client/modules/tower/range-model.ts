8c00a1f0202d52a9ab8b8a045813bfa729608741
AshwinHub <phantomfills@outlook.com>
2024-03-23 12:11:13 +0000

    🔨refresh range indicator model

--- game/src/client/modules/tower/range-model.ts
+++ game/src/client/modules/tower/range-model.ts
@@ -1,16 +1,26 @@
 export function createRangeModel(range: number, position: Vector3): Model {
 	const rangeIndicatorModel = new Instance("Model");
 
+	// Humanoid is required for the highlight to work with transparency - buggy Roblox instances
+	const humanoid = new Instance("Humanoid");
+	humanoid.Parent = rangeIndicatorModel;
+
 	const rangeIndicator = new Instance("Part");
 	rangeIndicator.Shape = Enum.PartType.Cylinder;
 	rangeIndicator.Size = new Vector3(0.1, range * 2, range * 2);
 	rangeIndicator.Anchored = true;
 	rangeIndicator.CanCollide = false;
-	rangeIndicator.Color = Color3.fromRGB(0, 163, 255);
+	rangeIndicator.Color = Color3.fromRGB(150, 150, 150);
 	rangeIndicator.Transparency = 0.5;
 	rangeIndicator.CFrame = new CFrame(position).mul(CFrame.Angles(0, 0, math.rad(90)));
 	rangeIndicator.CastShadow = false;
 	rangeIndicator.Parent = rangeIndicatorModel;
+
+	const highlight = new Instance("Highlight");
+	highlight.OutlineColor = Color3.fromRGB(255, 255, 255);
+	highlight.OutlineTransparency = 0;
+	highlight.FillTransparency = 1;
+	highlight.Parent = rangeIndicator;
 
 	return rangeIndicatorModel;
 }
