be3d1bd2c4f2210f889ad62040ac3572cd7a1148
AshwinHub <phantomfills@outlook.com>
2024-03-23 18:51:27 +0000

    🔨increase enemy speeds

--- game/src/shared/modules/enemy/enemy-type-to-enemy-stats-map.ts
+++ game/src/shared/modules/enemy/enemy-type-to-enemy-stats-map.ts
@@ -10,32 +10,32 @@ interface EnemyStats {
 const enemyTypeToStatsMap: Record<EnemyType, EnemyStats> = {
 	TRAINING_DUMMY: {
 		maxHealth: 30,
-		speed: 2,
+		speed: 3.5,
 		traits: [],
 	},
 	SPEEDSTER_DUMMY: {
 		maxHealth: 50,
-		speed: 3,
+		speed: 7.5,
 		traits: [],
 	},
 	ARMORED_DUMMY: {
 		maxHealth: 125,
-		speed: 1.5,
+		speed: 2.5,
 		traits: [],
 	},
 	STEALTH_DUMMY: {
 		maxHealth: 200,
-		speed: 3,
+		speed: 4.5,
 		traits: ["STEALTH"],
 	},
 	GUARD_DUMMY: {
 		maxHealth: 2_500,
-		speed: 1.5,
+		speed: 2,
 		traits: ["REINFORCED"],
 	},
 	DUMMY_TANK: {
 		maxHealth: 30_000,
-		speed: 1.25,
+		speed: 1.5,
 		traits: [],
 	},
 };
