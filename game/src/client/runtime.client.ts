0ecf412b2721c384a40841359ca6a42f79cf921a
AshwinHub <phantomfills@outlook.com>
2024-04-05 13:42:27 +0100

    🔨introduce commander admin panel

--- game/src/client/runtime.client.ts
+++ game/src/client/runtime.client.ts
@@ -1,11 +1,26 @@
 import { Flamework } from "@flamework/core";
+import { CommanderClient, CommanderInterface } from "@rbxts/commander";
+import { commanderEnemyType } from "shared/modules/command/enemy-type";
 
 Flamework.addPaths("src/client/components");
 Flamework.addPaths("src/client/controllers/app");
 Flamework.addPaths("src/client/controllers/enemy");
 Flamework.addPaths("src/client/controllers/tower");
 Flamework.addPaths("src/client/controllers/sound");
-Flamework.addPaths("src/client/controllers/admin");
 Flamework.addPaths("src/shared/components");
+
+CommanderClient.start(
+	(registry) => {
+		registry.registerType(commanderEnemyType);
+		registry.registerCommands();
+	},
+	{
+		interface: CommanderInterface.create({}),
+
+		historyLength: 1000,
+
+		registerBuiltInTypes: true,
+	},
+);
 
 Flamework.ignite();
--- game/src/server/runtime.server.ts
+++ game/src/server/runtime.server.ts
@@ -1,7 +1,20 @@
 import { Flamework } from "@flamework/core";
+import { CommanderServer } from "@rbxts/commander";
+import { commanderEnemyType } from "shared/modules/command/enemy-type";
 
 Flamework.addPaths("src/server/components");
 Flamework.addPaths("src/server/services");
 Flamework.addPaths("src/shared/components");
+Flamework.addPaths("src/server/modules/command");
+
+CommanderServer.start(
+	(registry) => {
+		registry.registerType(commanderEnemyType);
+		registry.registerCommands();
+	},
+	{
+		registerBuiltInTypes: true,
+	},
+).catch((err) => warn(`Commander could not be started: ${tostring(err)}`));
 
 Flamework.ignite();
